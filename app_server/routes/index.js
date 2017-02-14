dc07547d54518e2b53a470a516f4d4109380bb92
Almudena Konrad <akonrad@mills.edu>
2017-02-14 22:15:24 +0000

    finished changes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
+var labsix = require('../controllers/labsix');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
+router.get('/pageone', labsix.index);
 
 module.exports = router;
